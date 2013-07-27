8b7f31e07ed275a3cfee371776a9bd800fdc6581
Ayaz Maroof <ayaz.maroof.dev@gmail.com>
2013-07-27 08:58:02 +0000

    Using index.html for Deployment

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
+var fs = require('fs');
 
+app.use(express.logger());
+var buf = new Buffer(fs.readFileSync(index.html);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
